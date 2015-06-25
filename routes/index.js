dc65e73fb94718acb2fb1f677afe0cdb7386924c
Carlos Albaladejo <karlozalb@gmail.com>
2015-06-25 18:08:37 +0200

    Añadida primera pregunta y respuesta. Añadido enlace a dicha pregunta en página principal. Eliminados ficheros basura *.js~ u *.ejs~

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var quizControl = require('../controllers/quiz_controller.js');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'SuperQUIZ' });
 });
+
+router.get('/quizes/question',quizControl.question);
+router.get('/quizes/answer',quizControl.answer);
 
 module.exports = router;
